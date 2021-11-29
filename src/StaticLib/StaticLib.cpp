1af8d7c646db3cb8ee7d5863bebce0cf0aa25129
g2127043@st.t-kougei.ac.jp <g2127043@st.t-kougei.ac.jp>
2021-11-29 21:47:24 +0900

    テストを通しました。

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -6,7 +6,29 @@
 // クイックソート(引数が不適切であればfalseを返す)
 bool quick_sort(item* begin, const item* end)
 {
+	if (begin == NULL || end == NULL) return false;
+	if (end < begin) return false;
 	// ToDo: クイックソートで並び替えてください
-
-	return false;
+	int left = 0;
+	int right = (end - begin) - 1;
+	if (left == right) return true;
+	
+	while (1) {
+		int pivot = begin[(left + right) / 2].key;
+		int i = left;
+		int j = right;
+		while (1) {
+			while (begin[i].key < pivot) i++;
+			while (pivot < begin[j].key) j--;
+			if (i >= j) break;
+			item tmp = begin[i];
+			begin[i] = begin[j];
+			begin[j] = tmp;
+			i++;
+			j--;
+		}
+		if (left < i - 1) right = i - 1;
+		else if (j + 1 < right) left = j + 1;
+		else return true;
+	}
 }
