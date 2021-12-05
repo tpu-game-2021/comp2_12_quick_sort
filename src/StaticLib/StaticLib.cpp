d8604e33bb4be6bf4cd7744559c4f4db76885231
Harmony82 <seiyaxyz0918@gmail.com>
2021-12-05 16:04:53 +0900

    テストを通しました

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -7,6 +7,39 @@
 bool quick_sort(item* begin, const item* end)
 {
 	// ToDo: クイックソートで並び替えてください
-
-	return false;
+	if (end - begin < 0)return false;
+	if (end == NULL || begin == NULL)return false;
+	int pivot;
+	item tmp;
+	int i, j;
+	j =  (end - begin)-1;
+	i = 1;
+	tmp = begin[0];
+	begin[0] = begin[(end-begin)/2];
+	begin[(end - begin) / 2] = tmp;
+	pivot = begin[0].key;
+	if (end - begin <= 1)
+		return true;
+	while (1)
+	{
+		while (begin[j].key > pivot)
+			j--;
+		while (begin[i].key < pivot)
+		{
+			i++;
+		}
+		if (j <= i)
+			break;
+		tmp = begin[i];
+		begin[i] = begin[j];
+		begin[j] = tmp;
+		j--;
+		i++;
+	}
+	tmp = begin[j];
+	begin[j] = begin[0];
+	begin[0] = tmp;
+	quick_sort(begin, begin+j);
+	quick_sort(begin+j+1, end);
+	return true;
 }
