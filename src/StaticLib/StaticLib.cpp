308da1998e60a6717db5b7f3cd7253a77153da4c
g2127018 <ababababa3045@gmail.com>
2021-12-05 23:05:30 +0900

    テストを通すことが出来ませんでした。

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -3,10 +3,60 @@
 
 #include "../include/lib_func.h"
 
+/* 配列の要素を交換する */
+void Swap(int x[], int i, int j)
+{
+    int temp;
+
+    temp = x[i];
+    x[i] = x[j];
+    x[j] = temp;
+}
+
 // クイックソート(引数が不適切であればfalseを返す)
 bool quick_sort(item* begin, const item* end)
 {
 	// ToDo: クイックソートで並び替えてください
+
+    /*出来なかった
+    int i, j, x;
+    int k = 0;
+    int array_1[NUMBER_1];
+    int array_2[NUMBER_2];
+
+    for (i = 0; i < NUMBER_2; i++)
+    {
+
+        for (j = 0; j < NUMBER_2; j++)
+        {
+            array_2[i][j] = array_1[k];
+            k++;
+        }
+    }*/
+
+    int i, j, left, right;
+
+    int pivot;
+
+    i = left;                      /* ソートする配列の一番小さい要素の添字 */
+    j = right;                     /* ソートする配列の一番大きい要素の添字 */
+    
+    pivot = begin[(left + right) / 2].key; /* 基準値を配列の中央付近にとる */
+
+    while (1) {                    /* 無限ループ */
+
+        while (begin->key < pivot)       /* pivot より大きい値が */
+            i++;                   /* 出るまで i を増加させる */
+
+        while (pivot < begin->key)       /* pivot より小さい値が */
+            j--;                   /*  出るまで j を減少させる */
+        if (i >= j)                /* i >= j なら */
+            break;                 /* 無限ループから抜ける */
+
+        Swap(x, i, j);             /* x[i] と x[j]を交換 */
+        i++;                       /* 次のデータ */
+        j--;
+    }
 
 	return false;
 }
