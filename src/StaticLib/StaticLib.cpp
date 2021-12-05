23b9ea2f23a9dbf17bdbed1bef943f5341ec7cb3
SugarWind <g2127037@st.t-kougei.ac.jp>
2021-12-05 21:41:20 +0900

    テストを通しました。

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -6,7 +6,27 @@
 // クイックソート(引数が不適切であればfalseを返す)
 bool quick_sort(item* begin, const item* end)
 {
-	// ToDo: クイックソートで並び替えてください
+	// ToDo: クイックソートで並び替えてください	
+	if (begin == NULL || end == NULL) return false;
+	if (end < begin)  return false;
 
-	return false;
+	int left = 0;
+	int right = (end - begin)-1;
+
+	while (1)
+	{
+		int a = left;
+		int z = right;
+	    int pivot = begin[(a + z) / 2].key; //基準の値を設定する。
+
+		while (begin[a].key < pivot) a++; 
+		while (pivot < begin[z].key) z--; 
+		if (a >= z) break; 
+
+		item  tmp = begin[a];
+		begin[a] = begin[z];
+		begin[z] = tmp;
+		a++; z--;
+	}
+	return true;
 }
