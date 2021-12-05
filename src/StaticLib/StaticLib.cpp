e31d7c2a3f82b41bb87c49028c9459b1f799022b
Harmony82 <seiyaxyz0918@gmail.com>
2021-12-05 17:18:32 +0900

    テストを通しました

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -2,7 +2,13 @@
 #include "Windows.h"                    // Windows API の機能定義
 
 #include "../include/lib_func.h"
-
+void swap(item* a, item* b)
+{
+	item tmp;
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
 // クイックソート(引数が不適切であればfalseを返す)
 bool quick_sort(item* begin, const item* end)
 {
@@ -14,31 +20,32 @@ bool quick_sort(item* begin, const item* end)
 	int i, j;
 	j =  (end - begin)-1;
 	i = 1;
-	tmp = begin[0];
-	begin[0] = begin[(end-begin)/2];
-	begin[(end - begin) / 2] = tmp;
+	//tmp = begin[0];
+	//begin[0] = begin[(end-begin)/2];
+	//begin[(end - begin) / 2] = tmp;
+	swap(begin, begin+(end - begin) / 2);
 	pivot = begin[0].key;
 	if (end - begin <= 1)
 		return true;
 	while (1)
 	{
-		while (begin[j].key > pivot)
+		while (begin[j].key > pivot )
 			j--;
-		while (begin[i].key < pivot)
-		{
+		while (begin[i].key < pivot )
 			i++;
-		}
 		if (j <= i)
 			break;
-		tmp = begin[i];
-		begin[i] = begin[j];
-		begin[j] = tmp;
+		//tmp = begin[j];
+		//begin[j] = begin[i];
+		//begin[i] = tmp;
+		swap(begin + i, begin + j);
 		j--;
 		i++;
 	}
-	tmp = begin[j];
-	begin[j] = begin[0];
-	begin[0] = tmp;
+	//tmp = begin[j];
+	//begin[j] = begin[0];
+	//begin[0] = tmp;
+	swap(begin, begin+j);
 	quick_sort(begin, begin+j);
 	quick_sort(begin+j+1, end);
 	return true;
