2f47909d1813d169a11b93b1ada1dd1148f3e631
zakiAKR <zakican.ak@gmail.com>
2021-12-05 21:59:15 +0900

    テストコード通りました。

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -3,10 +3,33 @@
 
 #include "../include/lib_func.h"
 
+void swap(item a[], int i, int j)
+{
+	item box;
+	box = a[i];
+	a[i] = a[j];
+	a[j] = box;
+}
+
 // クイックソート(引数が不適切であればfalseを返す)
 bool quick_sort(item* begin, const item* end)
 {
 	// ToDo: クイックソートで並び替えてください
+	if (begin == NULL || end == NULL) return false;
+	if (end < begin) return false;
+	
+	unsigned int Lbox = 0;
+	unsigned int Rbox = (end - begin) - 1;
+	int average = begin[(Lbox + Rbox) / 2].key;
 
-	return false;
+	while (1)
+	{
+		while (begin[Lbox].key < average) Lbox++;
+		while (begin[Rbox].key > average) Rbox--;
+		if (Lbox >= Rbox) break;
+		swap(begin, Lbox, Rbox);
+		Lbox++;
+		Rbox--;
+	}
+	return true;
 }
