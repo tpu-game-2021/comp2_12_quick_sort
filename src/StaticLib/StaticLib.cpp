d77b2127cde2c591f600f84c7cd625d4a50d25cf
ryouta <ryouta32ryouta@gmail.com>
2021-12-05 13:51:45 +0900

    変更

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -4,9 +4,45 @@
 #include "../include/lib_func.h"
 
 // クイックソート(引数が不適切であればfalseを返す)
+void Swap(item x[], int i, int j) {
+	item tempo;
+
+	tempo = x[i];
+	x[i] = x[j];
+	x[j] = tempo;
+}
+
 bool quick_sort(item* begin, const item* end)
 {
-	// ToDo: クイックソートで並び替えてください
+	int left = 0;
+	int right = (end - begin) - 1;
 
-	return false;
+	int i = left;
+	int j = right;
+
+
+	if (end < begin)
+		return false;
+
+	if (end == NULL || begin == NULL)
+		return false;
+
+
+		while (1) {
+
+			int pivot = begin[(i + j) / 2].key;
+
+			while (begin[i].key < pivot)
+				i++;
+			while (pivot < begin[j].key)
+				j--;
+
+			if (i >= j)
+				break;
+
+			Swap(begin, i, j);
+			i++;
+			j--;
+		}
+	return true;
 }
