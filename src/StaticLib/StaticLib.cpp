b51800f11b959cb6eaa88257cfc4e5bdd2d9b7cf
suginokiSSS <1331issei@gmail.com>
2021-12-05 22:39:06 +0900

    テストを通せませんでした

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -7,6 +7,35 @@
 bool quick_sort(item* begin, const item* end)
 {
 	// ToDo: クイックソートで並び替えてください
+	if (begin == NULL || end == NULL)return false;
+	if (end < begin)return false;
 
-	return false;
+	item* it = begin;
+	item* it2 = begin;
+	int center = (begin->key + end->key) / 2;
+	
+	for (it2; it2 != end; it2++);
+
+	while (1)
+	{
+		while (it->key < center)it++;
+		while (it2->key > center)it2--;
+
+		if (it->key >= it2->key)break;
+
+		item tmp = *it;
+		*it = *it2;
+		*it2 = tmp;
+
+		it++;
+		it2--;
+	}
+
+	if (it->key - begin->key >= 2)
+		quick_sort(begin, it - 1);
+
+	if (end->key - it2->key >= 2)
+		quick_sort(it2 + 1, end);
+
+	return true;
 }
