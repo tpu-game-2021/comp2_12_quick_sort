b5073c7d2147056fb76bffb1601e3ba642848541
shun-chi-man <shun-shun.2002-0505@ezweb.ne.jp>
2021-12-05 21:10:50 +0900

    テストを通るようにしました。

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -7,6 +7,41 @@
 bool quick_sort(item* begin, const item* end)
 {
 	// ToDo: クイックソートで並び替えてください
+	if (begin == NULL || end == NULL)  return false;
+	
+	if (end < begin)  return false;
 
-	return false;
+	int left = 0;
+	int right = (end - begin) - 1;
+
+	if (left == right)  return true;
+
+	while (1)
+	{
+		int pivot = begin[(left + right) / 2].key;
+		int i = left;
+		int j = right;
+
+		while (1)
+		{
+			while (begin[i].key < pivot)  i++;
+			while (pivot < begin[j].key)  j--;
+
+			if (i >= j)  break;
+
+			item tmp = begin[i];
+			begin[i] = begin[j];
+			begin[j] = tmp;
+			i++;
+			j--;
+		}
+
+		if (left < i - 1)  right = i - 1;
+
+		else if (j + 1 < right)  left = j + 1;
+
+		else  break;
+	}
+
+	return true;
 }
